3abfa61b61ac6daec63f87427c261ade81a03e90
Kevin <carlisle1147@gmail.com>
2025-02-15 20:49:43 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 DEFAULT_HEADERS = {"User-Agent": USER_AGENT}
 
 # Number of pages to scrape on eBay (can be adjusted)
-SCRAPER_NUM_PAGES = 5
+SCRAPER_NUM_PAGES = 1
 
 # Outlier detection multiplier for the IQR method (default 1.5)
 OUTLIER_IQR_MULTIPLIER = 1.5
