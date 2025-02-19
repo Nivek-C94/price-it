1f86168001575dab72888c4d91d6606c8e6492d1
Nivek-C94 <carlisle1147@gmail.com>
2025-02-18 20:33:58 -0500

    Driver pool

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import os
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes import router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 # Include routes
 app.include_router(router)
 
+'''
+
+uvicorn main:app --host 0.0.0.0 --port 443 --ssl-keyfile ssl\privkey.pem --ssl-certfile "ssl\fullchain.pem"
+
+'''
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Use assigned port (e.g. Render's port)
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 80))  # Use assigned port (e.g. Render's port)
+    uvicorn.run(app, host="0.0.0.0", port=port, ssl_keyfile="ssl\\privkey.pem", ssl_certfile="ssl\\fullchain.pem")
